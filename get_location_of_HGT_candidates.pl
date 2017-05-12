2dab52b1213343d9ba0d611e0416a9ef855a97bf
reubwn <reubennowell@gmail.com>
2017-05-12 18:41:33 +0100

    update get_location_of_HGT_candidates.pl

--- get_location_of_HGT_candidates.pl
+++ get_location_of_HGT_candidates.pl
@@ -91,7 +91,7 @@ open (my $GFF, $gfffile) or die "[ERROR] Cannot open file $gfffile: $!\n";
 while (<$GFF>) {
   foreach my $gene (nsort keys %hgt_results) {
     if ( index($_, $gene)>=0 ) {
-      print join ("\t", $hgt_results{$k}{'scaffold'}, $k, $hgt_results{$k}{'hU'}, $hgt_results{$k}{'AI'}, "\n");
+      print join ("\t", $hgt_results{$gene}{'scaffold'}, $gene, $hgt_results{$gene}{'hU'}, $hgt_results{$gene}{'AI'}, "\n");
       last;
     }
   }
