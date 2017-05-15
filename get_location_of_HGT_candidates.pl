4c575692d8a0e93583421e08fa0e65703327ddad
reubwn <reubennowell@gmail.com>
2017-05-15 19:34:27 +0100

    improve speed

--- get_location_of_HGT_candidates.pl
+++ get_location_of_HGT_candidates.pl
@@ -20,25 +20,27 @@ OPTIONS:
   -u|--outgrp [INT]   : threshold hU score for determining 'good' OUTGROUP (HGT) genes [default>=30]
   -U|--ingrp  [INT]   : threshold hU score for determining 'good' INGROUP genes [default<=0]
   -y|--heavy  [FLOAT] : threshold for determining 'HGT heavy' scaffolds, with >= this proportion genes >= hU [default>=0.75]
+  -s|--subset [INT]   : test on a subset of proteins [for debug]
   -h|--help           : prints this help message
 
 OUTPUTS
   A '*.HGT_locations' file and a map file, and a list of scaffolds with 'too many' HGT candidates encoded on them to be believable ('*.HGT_heavy').
 \n";
 
-my ($infile,$namesfile,$gfffile,$prefix,$help);
+my ($infile,$namesfile,$gfffile,$subset,$help);
 my $outgrp = 30;
 my $ingrp = 0;
 my $heavy = 0.75;
 
 GetOptions (
-  'in|i=s'      => \$infile,
+  'i|in=s'      => \$infile,
   'n|names=s' => \$namesfile,
-  'gff|g=s'     => \$gfffile,
-  'outgrp|u:i'      => \$outgrp,
-  'ingrp|U:i'     => \$ingrp,
-  'heavy|y:f'   => \$heavy,
-  'help|h'      => \$help,
+  'g|gff=s'     => \$gfffile,
+  'u|outgrp:i'      => \$outgrp,
+  'U|ingrp:i'     => \$ingrp,
+  'y|heavy:f'   => \$heavy,
+  's|subset:i'  => \$subset,
+  'h|help'      => \$help,
 );
 
 die $usage if $help;
@@ -46,158 +48,143 @@ die $usage unless ($infile && $gfffile);
 
 print STDERR "[INFO] Infile: $infile\n";
 print STDERR "[INFO] GFF file: $gfffile\n";
-print STDERR "[INFO] Protein names file: $namesfile\n";
+print STDERR "[INFO] Proteins names file: $namesfile\n";
+print STDERR "[INFO] hU threshold to determine strong evidence for OUTGROUP: >= $outgrp\n";
+print STDERR "[INFO] hU threshold to determine strong evidence for INGROUP: <= $ingrp\n";
+print STDERR "[INFO] Proportion of genes >= hU threshold to determine 'HGT heavy' scaffolds: $heavy\n";
 
-(my $infilesize = `wc -l $infile`) =~ s/\s.+\n//;
-(my $namesfilesize = `wc -l $namesfile`) =~ s/\s.+\n//;
-(my $bedfile = $infile) =~ s/HGT_results.+/HGT_locations.bed.txt/;
-(my $locationsfile = $infile) =~ s/HGT_results.+/HGT_locations.txt/;
+(my $locationsfile = $infile) =~ s/HGT_results.+/HGT_locations.bed.txt/;
 (my $summaryfile = $infile) =~ s/HGT_results.+/HGT_locations.summary.txt/;
 (my $heavyfile = $infile) =~ s/HGT_results.+/HGT_locations.heavy.txt/;
-my (%bed,%query_names,%hgt_results,%scaffolds,%gff,%seen);
-my $n = 1;
+my ($namesfilesize,$isfasta,%bed,%query_names,%hgt_results,%scaffolds,%gff,%seen);
+my $n=1;
 
-## grep protein names from GFF and convert to BED:
+## autodetect if names are coming from fasta:
+if ($namesfile =~ m/(fa|faa|fasta)$/) {
+  print STDERR "[INFO] Proteins names file is fasta...\n";
+  $namesfilesize = `grep -c ">" $namesfile`;
+  $namesfilesize =~ s/\s.+\n//;
+  $isfasta = 1;
+} else {
+  $namesfilesize = `wc -l $namesfile`;
+  $namesfilesize =~ s/\s.+\n//;
+}
+
+## grep protein names from GFF and get coords of CDS:
 open (my $NA, $namesfile) or die "[ERROR] Cannot open $namesfile: $!\n";
-open (my $BED, ">$bedfile") or die "[ERROR] Cannot open $bedfile: $!\n";
+#open (my $BED, ">$bedfile") or die "[ERROR] Cannot open $bedfile: $!\n";
 while (my $gene = <$NA>) {
   chomp $gene;
   print STDERR "\r[INFO] Working on query \#$n: $gene (".percentage($n,$namesfilesize)."\%)"; $|=1;
 
   my ($start,$end,$chrom,$strand) = (1e+9,0,"NULL","NULL"); ##this will work so long as no start coord is ever >=1Gb!
+
+  ## get coords of all items grepped by $gene
   open (my $G, "grep -F $gene $gfffile |") or die "$!\n";
   while (<$G>) {
     chomp;
     my @F = split (/\s+/, $_);
-    $start = $F[3] if $F[3] < $start;
-    $end = $F[4] if $F[4] > $end;
+    $start = $F[3] if $F[3] < $start; ##then get ONLY the 1st
+    $end = $F[4] if $F[4] > $end; ##... and last coords across all items
     $chrom = $F[0];
     $strand = $F[6];
     $bed{$gene} = { ##key= gene; val= HoH
                     'chrom'  => $chrom,
-                    'start'  => $start,
-                    'end'    => $end,
+                    'start'  => $start, ##this should cover the 'gene region'
+                    'end'    => $end, ##... encoded by the protein name
                     'strand' => $strand
                    };
-    # my @bedline = ("\t", $F[0],$start,$end,$gene,".",$F[6],"\n");
-    # $bed{$F[0]} = \@bedline;
   }
   close $G;
+  print STDERR "\n";
 
   ## build scaffolds hash:
   push ( @{ $scaffolds{$chrom} }, $gene ); ##key= scaffold; val= \@array of genes on that scaffold
 
   $n++;
-  last if $n == 10000;
+  if ($subset) {last if $n == $subset};
 }
 close $NA;
-close $BED;
-$n = 0;
-#print Dumper \%bed;
 
 ## parse HGT_results file:
+print STDERR "[INFO] Parsing HGT_results file...\n";
 open (my $RESULTS, $infile) or die "[ERROR] Cannot open $infile: $!\n";
 while (<$RESULTS>) {
   chomp;
   next if /^\#/;
   my @F = split (/\s+/);
-  print STDERR "\r[INFO] Working on query \#$n: $F[0] (".percentage($n,$infilesize)."\%)"; $|=1;
 
-#  my @gffline = split(/\s+/, `grep -m 1 -F $F[0] $gfffile`); ##grep 1st line from GFF containing query name
-#  die "[ERROR] No scaffold name found for query $F[0]\n" if (scalar(@gffline)==0);
+  ## evaluate hU result:
+  my $hU_strength;
+  if ($F[3] <= $ingrp) {
+    $hU_strength = 0; ##good Metazoa candidate
+  } elsif ($F[3] >= $outgrp) {
+    $hU_strength = 2; ##good HGT candidate
+  } else {
+    $hU_strength = 1; ##intermediate bitscore difference
+  }
 
   ## build HGT_results hash:
   $hgt_results{$F[0]} = { ##key= query; val= HoH
-  #  'scaffold' => $gffline[0], ##the scaffold will be the first element in @gffline, assuming a normal GFF
     'hU'       => $F[3],
     'AI'       => $F[6],
     'bbsumcat' => $F[9],
     'CHS'      => $F[10],
-    'taxonomy' => $F[11]
+    'taxonomy' => $F[11],
+    'strength' => $hU_strength
   };
-
-  ## build scaffolds hash:
-  #push ( @{ $scaffolds{$gffline[0]} }, $F[0] ); ##key= scaffold; val= \@array of genes on that scaffold
-
-  $n++;
-  #last if percentage($n,$infilesize) == 0.1;
 }
 close $RESULTS;
-print STDERR "\n";
-print STDERR "[INFO] Number of queries: ".scalar(keys %hgt_results)."\n";
+print STDERR "[INFO] Number of queries in HGT_results: ".scalar(keys %hgt_results)."\n";
 print STDERR "[INFO] Mapping results to GFF...\n";
-$n=1;
+$n=0;
 
-## iterate through GFF:
+## iterate through pseudo-GFF:
 open (my $LOC, ">$locationsfile") or die "[ERROR] Cannot open file $locationsfile: $!\n";
-open (my $GFF, $gfffile) or die "[ERROR] Cannot open file $gfffile: $!\n";
-
+open (my $SUM, ">$summaryfile") or die "[ERROR] Cannot open file $summaryfile: $!\n";
+open (my $HEV, ">$heavyfile") or die "[ERROR] Cannot open file $heavyfile: $!\n";
+print $SUM join ("\t", "SCAFFOLD","GOOD_INGRP","INTERMEDIATE","GOOD_OUTGRP","PROPORTION_OUTGRP","IS_LINKED","\n");
+my ($is_linked_total,$is_heavy) = (0,0);
 foreach my $chrom (nsort keys %scaffolds) {
-  # foreach my $gene ( @{$scaffolds{$chrom}} ) { ##get genes on scaffold
-  #   print "$chrom\t$gene\t$bed{$gene}{start}\t$bed{$gene}{end}\t$hgt_results{$gene}{hU}\n";
-  # }
-  print "## Scaffold $chrom\n";
+  print STDERR "\r[INFO] Working on scaffold \#$n: $chrom (".percentage($n,scalar(keys %scaffolds))."\%)"; $|=1;
+  print $LOC "## Scaffold \#$n: $chrom\n";
+
+  ## sort by start coord within the %bed hash:
+  my ($good_outgrp,$good_ingrp,$intermediate,$na,$is_linked) = (0,0,0,0,0);
   foreach my $gene ( sort {$bed{$a}{start}<=>$bed{$b}{start}} @{$scaffolds{$chrom}} ) {
-    $hgt_results{$gene}{hU} = "NA" unless exists($hgt_results{$gene}{hU});
-    print "$chrom\t$gene\t$bed{$gene}{start}\t$bed{$gene}{end}\t$hgt_results{$gene}{hU}\n";
+    if ( exists($hgt_results{$gene}{hU}) ) {
+      if ($hgt_results{$gene}{strength} == 2) {
+        print $LOC join ("\t", $chrom,$bed{$gene}{start},$bed{$gene}{end},$gene,".",$bed{$gene}{strand},$hgt_results{$gene}{hU},$hgt_results{$gene}{strength},$hgt_results{$gene}{taxonomy},"\n");
+        $good_outgrp++;
+      } else {
+        print $LOC join ("\t", $chrom,$bed{$gene}{start},$bed{$gene}{end},$gene,".",$bed{$gene}{strand},$hgt_results{$gene}{hU},$hgt_results{$gene}{strength},"\n");
+        $good_ingrp++ if $hgt_results{$gene}{strength} == 0;
+        $intermediate++ if $hgt_results{$gene}{strength} == 1;
+      }
+    } else {
+      ## NA if either no hit or if hit to 'skipped' taxon (usually self-phylum):
+      print $LOC join ("\t", $chrom,$bed{$gene}{start},$bed{$gene}{end},$gene,".",$bed{$gene}{strand},"NA","\n");
+      $na++;
+    }
   }
+  ## evaluate if HGT candidate gene is encoded on a scaffold which also encodes a 'good_ingrp' gene:
+  $is_linked = 1 if (($good_outgrp+$good_ingrp) >= 2); ##must have at least one strong evidence for both
+  print $SUM join ("\t", $chrom,$good_ingrp,$intermediate,$good_outgrp,($good_outgrp/($intermediate+$good_ingrp)),$is_linked,"\n");
+  $is_linked_total += $is_linked;
 
+  ## evaluate proportion of HGT candidates per scaffold; print to 'heavy' if > threshold:
+  if (($good_outgrp/($intermediate+$good_ingrp)) > $heavy) {
+    print $HEV join ("\t", $chrom,$good_ingrp,$intermediate,$good_outgrp,($good_outgrp/($intermediate+$good_ingrp)),$is_linked,"\n");
+    $is_heavy++;
+  }
+$n++;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-# OUTER: while (<$GFF>) {
-#   chomp;
-#   my @F = split (/\s+/, $_);
-#
-
-
-
-  # INNER: foreach my $gene (nsort keys %hgt_results) {
-  #   print STDERR "\r[INFO] Working on query \#$n: $gene"; $|=1;
-  #   next OUTER if exists($seen{$gene});
-  #
-  #   ## iterate through all genes:
-  #   if ( index($_, $gene)>=0 ) { ##search for substring match
-  #     #unless (exists($seen{$gene})) {
-  #       if ($hgt_results{$gene}{'hU'} >= $outgrp) {
-  #         print $LOC join ("\t",$hgt_results{$gene}{'scaffold'},$gene,"OUTGROUP",$hgt_results{$gene}{'hU'},$hgt_results{$gene}{'AI'},$hgt_results{$gene}{'CHS'},$hgt_results{$gene}{'taxonomy'},"\n");
-  #       } elsif ($hgt_results{$gene}{'hU'} <= $ingrp) {
-  #         print $LOC join ("\t", $hgt_results{$gene}{'scaffold'},$gene,"INGROUP",$hgt_results{$gene}{'hU'},"\n");
-  #       } else {
-  #         print $LOC join ("\t", $hgt_results{$gene}{'scaffold'},$gene,"INTERMEDIATE",$hgt_results{$gene}{'hU'},"\n");
-  #       }
-  #
-  #       $n++;
-  #     #}
-  #     $seen{$gene} = (); ##prevents printing again on another GFF line
-  #     last INNER; ##quit the foreach loop
-  #   }
-  # }
-# }
-close $GFF;
 close $LOC;
-
-## iterate across all genes per each scaffold:
-# foreach my $scaff (nsort keys %scaffolds) {
-#   my @genes = @{ $scaffolds{$scaff} };
-#   foreach my $gene (@genes) {
-#     print join ("\t", $scaff, $gene, $hgt_results{$gene}{'hU'}, $hgt_results{$gene}{'AI'}, $hgt_results{$gene}{'CHS'});
-#   }
-# }
-#
-# print Dumper \%scaffolds;
-
+close $SUM;
+close $HEV;
 print STDERR "\n";
+print STDERR "[INFO] Number of scaffolds with HGT proportion >= $heavy: $is_heavy\n";
+print STDERR "[INFO] Number of 'good' HGT candidates encoded on same scaffold as 'good' INGROUP gene: $is_linked_total\n";
 print STDERR "[INFO] Finished on ".`date`."\n";
 
 ################################################################################
