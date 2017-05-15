d02333299c5501693b36e57e6280880b8bcbcd4c
reubwn <reubennowell@gmail.com>
2017-05-15 17:00:17 +0100

    improve speed

--- get_location_of_HGT_candidates.pl
+++ get_location_of_HGT_candidates.pl
@@ -135,9 +135,8 @@ open (my $GFF, $gfffile) or die "[ERROR] Cannot open file $gfffile: $!\n";
 foreach my $chrom (nsort keys %scaffolds) {
   my @genes_all = @{ $scaffolds{$chrom} }; ##get genes on scaffold
   my @genes_hU = $hgt_results{@genes_all}{'hU'};
-  for (my $i=0; $i++; $i<=scalar(@genes_all)) {
-    print "$genes_all[$i]\t$genes_hU[$i]\n";
-  }
+  print "@genes_all\n";
+  print "@genes_hU\n";
   # my @genes_ingrp = grep { $hgt_results{$_}{'hU'} <= $ingrp } @genes_all;
   # my @genes_intermediate = grep { $hgt_results{$_}{'hU'} > $ingrp && $hgt_results{$_}{'hU'} < $outgrp } @genes_all;
 }
