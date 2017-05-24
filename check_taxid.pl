221fcaa9cc8a3b448c677c73090dc5c36f0b431a
reubwn <reubennowell@gmail.com>
2017-05-24 17:44:11 +0100

    check

---
+++ check_taxid.pl
@@ -0,0 +1,221 @@
+#!/usr/bin/env perl
+
+## author: reubwn Nov 2016
+## added hU functionality May 2017
+
+use strict;
+use warnings;
+
+use Getopt::Long;
+use Sort::Naturally;
+use Data::Dumper qw(Dumper);
+use List::Util qw(reduce sum min max);
+
+my ($taxid,$path);
+my $taxid_skip = 42241;
+my $taxid_threshold = 33208; ##metazoa
+
+GetOptions (
+  't|taxid=s'             => \$taxid,
+  'path|p:s'              => \$path,
+);
+
+## parse nodes and names:
+my (%nodes_hash, %names_hash, %rank_hash);
+if ($path) {
+  print STDERR "[INFO] Building taxonomy databases from tax files in '$path'...";
+  open(my $NODES, "$path/nodes.dmp") or die $!;
+  while (<$NODES>) {
+    chomp;
+    next if /\#/;
+    my @F = map { s/^\s+|\s+$//gr } split (/\|/, $_); ## split nodes.dmp file on \s+|\s+ regex
+    $nodes_hash{$F[0]} = $F[1]; ## key= child taxid; value= parent taxid
+    $rank_hash{$F[0]} = $F[2]; ## key= taxid; value= rank
+  }
+  close $NODES;
+  open (my $NAMES, "$path/names.dmp") or die $!;
+  while (<$NAMES>) {
+    chomp;
+    next if /\#/;
+    my @F = map { s/^\s+|\s+$//gr } split (/\|/, $_);
+    $names_hash{$F[0]} = $F[1] if ($F[3] eq "scientific name"); ## key= taxid; value= species name
+  }
+  close $NAMES;
+  if (-e "$path/merged.dmp") {
+    open (my $MERGED, "$path/merged.dmp") or die $!;
+    while (<$MERGED>) {
+      chomp;
+      next if /\#/;
+      my @F = map { s/^\s+|\s+$//gr } split (/\|/, $_);
+      $nodes_hash{$F[0]} = $F[1]; ## key= old taxid; value= new taxid
+      ## this will behave as if old taxid is a child of the new one, which is OK I guess
+    }
+  }
+}
+
+## print some info to STDERR:
+print STDERR " done\n";
+print STDERR "[INFO] Nodes parsed: ".scalar(keys %nodes_hash)."\n";
+print STDERR "[INFO] Threshold taxid set to '$taxid_threshold' ($names_hash{$taxid_threshold})\n";
+print STDERR "[INFO] INGROUP set to '$names_hash{$taxid_threshold}'; OUTGROUP is therefore 'non-$names_hash{$taxid_threshold}'\n";
+
+if ($taxid !~ m/\d+/) {
+  print ("$taxid: invalid/unrecognised taxid\n");
+  next;
+} elsif (check_taxid_has_parent($taxid) == 1) {
+  print ("$taxid: invalid/unrecognised parent taxid\n");
+  next;
+} elsif ( tax_walk($taxid, $taxid_skip) eq "ingroup" ) { ## do not include any hits to within taxid $taxid_skip
+  print ("$taxid: taxid within skipped ($taxid_skip)\n");
+  next;
+} elsif ( tax_walk($taxid) eq "unassigned" ) {
+  print ("$taxid: taxid unassigned/unclassified\n");
+  next;
+} else {
+  print tax_walk($taxid)."\n";
+  print tax_walk_to_get_rank_to_phylum($taxid)."\n";
+  print tax_walk_to_get_rank_to_species($taxid)."\n";
+}
+
+############################################ SUBS
+
+sub check_taxid_has_parent {
+  my $taxid = $_[0];
+  my $result = 0;
+  unless ($nodes_hash{$taxid}) {
+    $result = 1;
+  }
+  return $result; ## 0 = taxid exists; 1 = taxid does not exist
+}
+
+sub tax_walk {
+    my $taxid = $_[0];
+    my $walk_to;
+    if (exists $_[1]) {
+      $walk_to = $_[1];
+    } else {
+      $walk_to = $taxid_threshold; ## default is metazoa
+    }
+
+    ## first parent:
+    my $parent = $nodes_hash{$taxid};
+    my $result;
+
+    ## return "unassigned" if hit has no valid taxid
+    if ($parent !~ m/\d+/) {
+      $result = "unassigned";
+      return $result;
+    }
+
+    ## recurse the tree:
+    while (1) {
+      if ($parent == $walk_to) {
+        $result = "ingroup"; ## is ingroup
+        last;
+      } elsif ($parent == 1) {
+        $result = "outgroup"; ## root; i.e., the whole tree has been recursed without finding $threshold, therefore $taxid must reside in another part of the tax tree
+        last;
+      } elsif ($parent == 32644) {
+        $result = "unassigned"; ## taxid for "unidentified"
+        last;
+      } elsif ($parent == 12908) {
+        $result = "unassigned"; ## taxid for "unclassified sequences"
+        last;
+      } else { ## walk up the tree!
+        $parent = $nodes_hash{$parent};
+      }
+    }
+    return $result;
+}
+
+sub tax_walk_to_get_rank_to_phylum {
+  my $taxid = $_[0];
+  my $parent = $nodes_hash{$taxid};
+  my $parent_rank = $rank_hash{$parent};
+  my ($phylum,$kingdom,$superkingdom) = ("undef","undef","undef");
+
+  while (1) {
+    if ($parent_rank eq "phylum") {
+      $phylum = $names_hash{$parent};
+      #print "Found phylum: $phylum\n";
+      $parent = $nodes_hash{$parent};
+      $parent_rank = $rank_hash{$parent};
+      next;
+    } elsif ($parent_rank eq "kingdom") {
+      $kingdom = $names_hash{$parent};
+      #print "Found phylum: $kingdom\n";
+      $parent = $nodes_hash{$parent};
+      $parent_rank = $rank_hash{$parent};
+      next;
+    } elsif ($parent_rank eq "superkingdom") {
+      $superkingdom = $names_hash{$parent};
+      #print "Found phylum: $superkingdom\n";
+      last;
+    } elsif ($parent == 1) {
+      last;
+    } else {
+      $parent = $nodes_hash{$parent};
+      $parent_rank = $rank_hash{$parent};
+    }
+  }
+  my $result = join (";",$superkingdom,$kingdom,$phylum);
+  $result =~ s/\s+/\_/g; ## replace spaces with underscores
+  return $result;
+}
+
+sub tax_walk_to_get_rank_to_species {
+  my $taxid = $_[0];
+  my $parent = $nodes_hash{$taxid};
+  my $parent_rank = $rank_hash{$parent};
+  my ($species,$genus,$family,$order,$class,$phylum,$kingdom,$superkingdom) = ("undef","undef","undef","undef","undef","undef","undef","undef");
+
+  while (1) {
+    if ($parent_rank eq "species") {
+      $species = $names_hash{$parent};
+      $parent = $nodes_hash{$parent};
+      $parent_rank = $rank_hash{$parent};
+      next;
+    } elsif ($parent_rank eq "genus") {
+      $genus = $names_hash{$parent};
+      $parent = $nodes_hash{$parent};
+      $parent_rank = $rank_hash{$parent};
+      next;
+    } elsif ($parent_rank eq "family") {
+      $family = $names_hash{$parent};
+      $parent = $nodes_hash{$parent};
+      $parent_rank = $rank_hash{$parent};
+      next;
+    } elsif ($parent_rank eq "order") {
+      $order = $names_hash{$parent};
+      $parent = $nodes_hash{$parent};
+      $parent_rank = $rank_hash{$parent};
+      next;
+    } elsif ($parent_rank eq "class") {
+      $class = $names_hash{$parent};
+      $parent = $nodes_hash{$parent};
+      $parent_rank = $rank_hash{$parent};
+      next;
+    } elsif ($parent_rank eq "phylum") {
+      $phylum = $names_hash{$parent};
+      $parent = $nodes_hash{$parent};
+      $parent_rank = $rank_hash{$parent};
+      next;
+    } elsif ($parent_rank eq "kingdom") {
+      $kingdom = $names_hash{$parent};
+      $parent = $nodes_hash{$parent};
+      $parent_rank = $rank_hash{$parent};
+      next;
+    } elsif ($parent_rank eq "superkingdom") {
+      $superkingdom = $names_hash{$parent};
+      last;
+    } elsif ($parent == 1) {
+      last;
+    } else {
+      $parent = $nodes_hash{$parent};
+      $parent_rank = $rank_hash{$parent};
+    }
+  }
+  my $result = join (";",$superkingdom,$kingdom,$phylum,$class,$order,$family,$genus,$species);
+  $result =~ s/\s+/\_/g; ## replace spaces with underscores
+  return $result;
+}
