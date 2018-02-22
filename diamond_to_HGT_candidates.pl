48c28028c24ec41d4f0f334e0f26353e3169d64a
reubwn <reubennowell@gmail.com>
2018-02-22 19:19:50 +0000

    update

--- diamond_to_HGT_candidates.pl
+++ diamond_to_HGT_candidates.pl
@@ -416,12 +416,13 @@ close $HGT;
 close $WARN;
 
 print STDERR "\r[INFO] Processed ".commify($processed)." queries\n";
-print STDERR "[INFO] TOTAL NUMBER OF HGT CANDIDATES: ".commify(scalar(keys(%hgt_candidates)))."\n";
-print STDERR "[INFO] Number of queries with HGT Index (hU) >= $hU_threshold: ".commify($hU_supported)."\n";
-print STDERR "[INFO] Number of queries with Alien Index (AI) >= $hU_threshold: ".commify($AI_supported)."\n";
-print STDERR "[INFO] Number of queries in OUTGROUP category ('non-$names_hash{$taxid_threshold}'): ".commify($outgroup)."\n";
-print STDERR "[INFO] Number of queries in OUTGROUP category ('non-$names_hash{$taxid_threshold}') with support >= $support_threshold\%: ".commify($outgroup_supported)."\n";
-print STDERR "[INFO] Number of queries in unassigned/unclassified category: ".commify($unassigned)."\n" if $unassigned > 0;
+#print STDERR "[INFO] Number of queries in unassigned/unclassified category: ".commify($unassigned)."\n" if $unassigned > 0;
+#print STDERR "[INFO] Number of queries in OUTGROUP category ('non-$names_hash{$taxid_threshold}'): ".commify($outgroup)."\n";
+#print STDERR "[INFO] Number of queries in OUTGROUP category ('non-$names_hash{$taxid_threshold}') with CHS >= $support_threshold\%: ".commify($outgroup_supported)."\n";
+print STDERR "[INFO] Number of queries with HGT Index >= $hU_threshold: ".commify($hU_supported)."\n";
+print STDERR "[INFO] Number of queries with HGT Index >= $hU_threshold and CHS >= $support_threshold\% to non-$names_hash{$taxid_threshold}: ".commify(scalar(keys(%hgt_candidates)))."\n";
+#print STDERR "[INFO] Number of queries with Alien Index (AI) >= $hU_threshold: ".commify($AI_supported)."\n";
+#print STDERR "[INFO] NUMBER OF HGT CANDIDATES: ".commify(scalar(keys(%hgt_candidates)))."\n";
 print STDERR "[INFO] Finished on ".`date`."\n";
 
 ############################################ SUBS
