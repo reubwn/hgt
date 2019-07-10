3a0c31691d5ecf2f03eee29f8913809c98089ac5
Reuben Nowell <rnowell@Reubens-MBP.domain_not_set.invalid>
2019-07-10 18:44:21 +0200

    update

--- get_location_of_HGT_candidates.pl
+++ get_location_of_HGT_candidates.pl
@@ -141,8 +141,8 @@ if ( ($names_file =~ m/(fa|faa|fasta)$/) or ($names_file =~ m/(fa.gz|faa.gz|fast
       my ($start,$end,$introns) = (1e+12,0,-1); ## this will work so long as no start coord is ever >=1Tb!
       my ($chrom,$strand) = ("NULL","NULL");
       ## get coords of all items grepped by $gene
-      foreach ( grep { m/\Q$gene\;\E/ } @GFF_array ) { ## assumes $gene is bounded by a ';'
-        print STDOUT "\nI found: $_\n";
+      foreach ( grep { m/\Q$gene\E\;/ } @GFF_array ) { ## assumes $gene is bounded by a ';'
+        print STDOUT "\nGrepped for '$gene': $_\n" if ( $debug );
         chomp;
         my @F = split (/\s+/, $_);
         $start = $F[3] if $F[3] < $start; ##then get ONLY the 1st
